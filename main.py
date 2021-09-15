8cd4a25167d9406c749ce7f132632de13285e911
masterharoldsaxon4@gmail.com <masterharoldsaxon4@gmail.com>
2021-09-15 17:03:09 +0300

    add getChatsList and get_chat_invite_link

--- main.py
+++ main.py
@@ -4,8 +4,77 @@ from pyrogram import Client, idle
 from flask import request
 import json
 import time
+from pyrogram.raw import functions
+from pyrogram import types
+from pyrogram import utils
+from pyrogram.file_id import FileType
+from pyrogram.scaffold import Scaffold
+
 appTG = Client("my_account")
 app = Flask(__name__)
+
+
+def get_chats():
+    z = []
+    try:
+        time.sleep(0.1)
+        for dialog in appTG.iter_dialogs():
+            z.append({
+                "id": dialog.chat.id,
+                "name": dialog.chat.title or dialog.chat.first_name,
+                "type": dialog.chat.type,
+                "membercount": dialog.chat.members_count or 0,
+
+            })
+
+    except:
+        return "Error"
+    return z
+
+
+def get_chat_invite_link(id):
+    m = None
+    typeM = None
+    try:
+        time.sleep(0.1)
+        print("id: ", id)
+        z = appTG.get_chat(id)
+        # x = appTG.send(
+        #     functions.messages.CheckChatInvite(
+        #         hash='t.me/joinchat/Ces3Vmef1Y1iOGNi'
+        #     )
+        # )
+        # print("X in: ", type(z))
+        tp = type(z)
+        if tp == types.ChatPreview:
+            # print("types.ChatPreview")
+            typeM = "ChatPreview"
+        elif tp == types.Chat:
+            # print("types.Chat")
+            typeM = "Chat"
+        id = getattr(z, 'id', 0)
+
+        m = {
+
+            "id":  id,
+            "typegroup": z.type,
+            "title": z.title,
+            "members_count": z.members_count,
+            "typelink": typeM,
+        }
+        # for dialog in appTG.iter_dialogs():
+        #     z.append({
+        #         "id": dialog.chat.id,
+        #         "name": dialog.chat.title or dialog.chat.first_name,
+        #         "type": dialog.chat.type,
+        #         "membercount": dialog.chat.members_count or 0,
+
+        #     })
+
+    except Exception as e:
+        print("e: ", e)
+        return "Error", str(e)
+    return m, None
 
 
 def get_member(chat_id, offset):
@@ -35,9 +104,9 @@ def get_count(id):
     err = ""
     m = None
     try:
-        print("Mxx")
+       # print("Mxx")
         m = appTG.get_chat_members_count(id)
-        print("M:", m)
+       # print("M:", m)
     except:
         print("MxxErr")
         err = m
@@ -45,14 +114,38 @@ def get_count(id):
     return m, err
 
 
+@app.route('/getchats')
+def handle_get_chats():
+    chats = get_chats()
+    if chats == "Error":
+        jsonStr = json.dumps({'chats': None, 'error': 'error get chats list'})
+        return jsonStr, 200, {'Content-Type': 'application/json'}
+    jsonStr = json.dumps({'chats': chats, 'error': None})
+
+    return jsonStr, 200, {'Content-Type': 'application/json'}
+
+
+@app.route('/getchatinvitelink')
+def handle_get_chat_invite_link():
+
+    chat_id = request.args.get('id')
+    chats, e = get_chat_invite_link(chat_id)
+    if chats == "Error":
+        jsonStr = json.dumps({'info': None, 'error': e})
+        return jsonStr, 200, {'Content-Type': 'application/json'}
+    jsonStr = json.dumps({'info': chats, 'error': None})
+
+    return jsonStr, 200, {'Content-Type': 'application/json'}
+
+
 @app.route('/')
-def hello_world():
+def handle_hello_world():
     appTG.send_message("me", "Hello from route")
     return 'Hello World!'
 
 
 @app.route('/getchatmemberscount')
-def get_num():
+def handle_get_num():
     chat_id = request.args.get('id')
     count, err = get_count(chat_id)
     if count == "Error":
@@ -64,7 +157,7 @@ def get_num():
 
 
 @app.route('/getchatmembers')
-def get_info():
+def handle_get_info():
     chat_id = request.args.get('id')
     count, err = get_count(chat_id)
     if count == "Error":
@@ -75,7 +168,7 @@ def get_info():
     n = c
     offset = 0
     chatList = []
-    print("N:", n)
+    #print("N:", n)
     for i in range(1, n + 1):
       #  print("cicle:", i)
         zm, err = get_member(chat_id, offset)
@@ -88,7 +181,7 @@ def get_info():
             return jsonStr, 200, {'Content-Type': 'application/json'}
         else:
             chatList = [*chatList, *zm]
-    print("slice len=", len(chatList))
+   # print("slice len=", len(chatList))
     jsonStr = json.dumps(chatList)
     return jsonStr, 200, {'Content-Type': 'application/json'}
 
